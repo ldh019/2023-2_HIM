7aefc40628545a57af13a6ef0a622d6dce23cd29
hoon <ldh019@naver.com>
2023-11-10 18:03:16 +0900

    그림 정리 및 코드 정리

--- main.py
+++ main.py
@@ -2,55 +2,59 @@ from PIL import Image
 from matplotlib import patches
 from matplotlib import pyplot as plt
 import numpy as np
+import sys
 
 
-def my_padding(img, shape):
-    pad_sizeY, pad_sizeX = int(shape[0] / 2), int(shape[1] / 2)
-    res = np.pad(img, [(pad_sizeY, shape[0] - pad_sizeY), (pad_sizeX, shape[1] - pad_sizeX)], 'constant')
+def my_padding(image, shape):
+    h, w = shape
+    pad_h, pad_w = int(h / 2), int(w / 2)
+    res = np.pad(image, [(pad_h, h - pad_h), (pad_w, w - pad_w)], 'constant')
     return res
 
 
 def my_kernel(shape, sigma):
-    a = shape[0]
-    b = shape[1]
+    a, b = shape
     y, x = np.ogrid[-b:b + 1, -a:a + 1]
+
     gaus_kernel = np.exp(-(x * x + y * y) / (2. * sigma * sigma))
     sum = gaus_kernel.sum()
     gaus_kernel /= sum
+
     return gaus_kernel
 
 
-def my_filtering(img, kernel):
-    row, col = len(img), len(img[0])
-    ksizeY, ksizeX = kernel.shape[0], kernel.shape[1]
+def my_filtering(image, kernel):
+    h, w = image.shape
+    kernel_h, kernel_w = kernel.shape
 
-    pad_image = my_padding(img, (ksizeY, ksizeX))
-    filtered_img = np.zeros((row, col), dtype=np.float32)
+    padded = my_padding(image, (kernel_h, kernel_w))
+    filtered = np.zeros((h, w), dtype=np.float32)
 
-    for i in range(row):
-        for j in range(col):
-            filtered_img[i, j] = np.sum(
-                np.multiply(kernel, pad_image[i:i + ksizeY, j:j + ksizeX]))
+    for i in range(h):
+        for j in range(w):
+            filtered[i, j] = np.sum(padded[i:i + kernel_h, j:j + kernel_w] * kernel)
 
-    return filtered_img
+    return filtered
 
 
 def get_pixel_data(name):
     gray = np.array(Image.open(name).convert("L"))
     h, w = gray.shape
-    gaus1 = my_kernel((5, 5), 1.6)
-    gaus2 = my_kernel((5, 5), 1)
-    gaussian1 = my_filtering(gray, gaus1)
-    gaussian2 = my_filtering(gray, gaus2)
-    DoG = np.zeros_like(gray)
+
+    kernel1 = my_kernel((5, 5), 1.6)
+    kernel2 = my_kernel((5, 5), 1)
+
+    filtered1 = my_filtering(gray, kernel1)
+    filtered2 = my_filtering(gray, kernel2)
+
+    dog = np.zeros_like(gray)
     for i in range(h):
         for j in range(w):
-            DoG[i][j] = float(gaussian1[i][j]) - float(gaussian2[i][j])
-    result = np.array(DoG).reshape(1, h, w)
+            dog[i][j] = filtered1[i][j] - filtered2[i][j]
 
-    result = (result - np.min(result)) / (np.max(result) - np.min(result))
+    normalized = (dog - np.min(dog)) / (np.max(dog) - np.min(dog))
 
-    return result
+    return normalized
 
 
 def get_pixel_data_target(name):
@@ -59,50 +63,34 @@ def get_pixel_data_target(name):
     return result
 
 
-def calc_convolution(X, filters):
-    _, h, w = X.shape
-    filters = np.flip(filters)
-    filter_c, filter_h, filter_w = filters.shape
+def calc_convolution(image, filter):
+    h, w = image.shape
+    filter = np.flip(filter)
+    filter_h, filter_w = filter.shape
 
-    out_h = h
-    out_w = w
+    image = my_padding(image, (filter_h, filter_w))
+    result = np.zeros((h, w))
 
-    in_X = my_padding(X[0], (filter_h, filter_w))
-    out = np.zeros((out_h, out_w))
+    for now_h in range(h):
+        start_h = now_h
+        end_h = start_h + filter_h
+        for now_w in range(w):
+            start_w = now_w
+            end_w = start_w + filter_w
+            result[now_h, now_w] = np.sum(image[start_h:end_h, start_w:end_w] * filter)
 
-    for c in range(filter_c):
-        for h in range(out_h):
-            h_start = h
-            h_end = h_start + filter_h
-            for w in range(out_w):
-                w_start = w
-                w_end = w_start + filter_w
-                out[h, w] = np.sum(in_X[h_start:h_end, w_start:w_end] * filters[c])
-
-    return out
+    return result
 
 
-def firstRgbResult(name, icon_name):
-    image = np.array(Image.open(name).convert("L"))
-    target = np.array(Image.open(icon_name).convert("L"))
-    target = np.flip(target)
+def finalResult(image_name, target_name):
+    origin = np.array(Image.open(image_name).convert("L"))
+    image = get_pixel_data(image_name)
+    target = get_pixel_data_target(target_name)
 
     conv = calc_convolution(image, target)
 
     plt.figure()
-    plt.imshow(conv, cmap='jet')
-    plt.colorbar()
-    plt.show()
-
-
-def finalResult(name, icon_name):
-    origin = np.array(Image.open(name).convert("L"))
-    digitized = get_pixel_data(name)
-    target = get_pixel_data_target(icon_name)
-    conv = calc_convolution(digitized, target)
-
-    plt.figure()
-    plt.imshow(digitized[0], cmap='gray')
+    plt.imshow(image, cmap='gray')
     plt.colorbar()
     plt.show()
 
@@ -112,8 +100,8 @@ def finalResult(name, icon_name):
     for index in indices:
         ax.add_patch(
             patches.Rectangle(
-                (index[1] - target.shape[2] // 2, index[0] - target.shape[1] // 2),
-                target.shape[2], target.shape[1],
+                (index[1] - target.shape[1] // 2, index[0] - target.shape[0] // 2),
+                target.shape[1], target.shape[0],
                 edgecolor='cyan',
                 fill=False,
             ))
@@ -124,9 +112,12 @@ def finalResult(name, icon_name):
     plt.show()
 
 
-name = 'image1.png'
-icon_name = 'target1.png'
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('이미지 파일의 이름과 아이콘 파일의 이름을 입력해야 합니다.')
+        print('Ex) python main.py image1.png target1.png')
+    else:
+        name = sys.argv[1]
+        icon_name = sys.argv[2]
 
-# firstRgbResult(name, icon_name)
-
-finalResult(name, icon_name)
+        finalResult(name, icon_name)
