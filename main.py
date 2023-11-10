78851e3735fb595cdaeb1dddd70963ebbc3f1bf3
hoon <ldh019@naver.com>
2023-11-10 16:12:34 +0900

    진짜 끝

--- main.py
+++ main.py
@@ -1,50 +1,16 @@
-import matplotlib.colors
 from PIL import Image
 from matplotlib import patches
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-def my_padding(img, shape, boundary=0):
-    row, col = len(img), len(img[0])
-    pad_sizeY, pad_sizeX = shape[0] // 2, shape[1] // 2
-    res = np.zeros((row + (2 * pad_sizeY), col + (2 * pad_sizeX)), dtype=np.float_)
-    pad_row, pad_col = len(res), len(res[0])
-    if pad_sizeY == 0:
-        res[pad_sizeY:, pad_sizeX:-pad_sizeX] = img.copy()
-    elif pad_sizeX == 0:
-        res[pad_sizeY:-pad_sizeY, pad_sizeX:] = img.copy()
-    else:
-        res[pad_sizeY:-pad_sizeY, pad_sizeX:-pad_sizeX] = img.copy()
-    if boundary == 0:
-        return res
-    elif boundary == 1:
-        res[0:pad_sizeY, 0:pad_sizeX] = img[0, 0]
-        res[-pad_sizeY:, 0:pad_sizeX] = img[row - 1, 0]
-        res[0:pad_sizeY, -pad_sizeX:] = img[0, col - 1]
-        res[-pad_sizeY:, -pad_sizeX:] = img[row - 1, col - 1]
-
-        res[0:pad_sizeY, pad_sizeX:pad_col - pad_sizeX] = np.repeat(img[0:1, 0:], [pad_sizeY], axis=0)  # 상단
-        res[pad_row - pad_sizeY:, pad_sizeX:pad_col - pad_sizeX] = np.repeat(img[row - 1:row, 0:], [pad_sizeY],
-                                                                             axis=0)  # 하단
-        res[pad_sizeY:pad_row - pad_sizeY, 0:pad_sizeX] = np.repeat(img[0:, 0:1], [pad_sizeX], axis=1)  # 좌측
-        res[pad_sizeY:pad_row - pad_sizeY, pad_col - pad_sizeX:] = np.repeat(img[0:, col - 1:col], [pad_sizeX],
-                                                                             axis=1)  # 우측
-        return res
-    else:
-        res[0:pad_sizeY, 0:pad_sizeX] = np.flip(img[0:pad_sizeY, 0:pad_sizeX])  # 좌측 상단
-        res[-pad_sizeY:, 0:pad_sizeX] = np.flip(img[-pad_sizeY:, 0:pad_sizeX])  # 좌측 하단
-        res[0:pad_sizeY, -pad_sizeX:] = np.flip(img[0:pad_sizeY, -pad_sizeX:])  # 우측 상단
-        res[-pad_sizeY:, -pad_sizeX:] = np.flip(img[-pad_sizeY:, -pad_sizeX:])  # 우측 하단
-
-        res[pad_sizeY:pad_row - pad_sizeY, 0:pad_sizeX] = np.flip(img[0:, 0:pad_sizeX], 1)  # 좌측
-        res[pad_sizeY:pad_row - pad_sizeY, pad_col - pad_sizeX:] = np.flip(img[0:, col - pad_sizeX:], 1)  # 우측
-        res[0:pad_sizeY, pad_sizeX:pad_col - pad_sizeX] = np.flip(img[0:pad_sizeY, 0:], 0)  # 상단
-        res[pad_row - pad_sizeY:, pad_sizeX:pad_col - pad_sizeX] = np.flip(img[row - pad_sizeY:, 0:], 0)  # 하단
-        return res
+def my_padding(img, shape):
+    pad_sizeY, pad_sizeX = int(shape[0] / 2), int(shape[1] / 2)
+    res = np.pad(img, [(pad_sizeY, shape[0] - pad_sizeY), (pad_sizeX, shape[1] - pad_sizeX)], 'constant')
+    return res
 
 
-def my_getGKernel(shape, sigma):
+def my_kernel(shape, sigma):
     a = shape[0]
     b = shape[1]
     y, x = np.ogrid[-b:b + 1, -a:a + 1]
@@ -54,11 +20,11 @@ def my_getGKernel(shape, sigma):
     return gaus_kernel
 
 
-def my_filtering(img, kernel, boundary=0):
+def my_filtering(img, kernel):
     row, col = len(img), len(img[0])
     ksizeY, ksizeX = kernel.shape[0], kernel.shape[1]
 
-    pad_image = my_padding(img, (ksizeY, ksizeX), boundary=boundary)
+    pad_image = my_padding(img, (ksizeY, ksizeX))
     filtered_img = np.zeros((row, col), dtype=np.float32)
 
     for i in range(row):
@@ -72,8 +38,8 @@ def my_filtering(img, kernel, boundary=0):
 def get_pixel_data(name):
     gray = np.array(Image.open(name).convert("L"))
     h, w = gray.shape
-    gaus1 = my_getGKernel((5, 5), 1.6)
-    gaus2 = my_getGKernel((5, 5), 1)
+    gaus1 = my_kernel((5, 5), 1.6)
+    gaus2 = my_kernel((5, 5), 1)
     gaussian1 = my_filtering(gray, gaus1)
     gaussian2 = my_filtering(gray, gaus2)
     DoG = np.zeros_like(gray)
@@ -88,31 +54,20 @@ def get_pixel_data(name):
 
 
 def get_pixel_data_target(name):
-    gray = np.array(Image.open(name).convert("L"))
-    gray_flip = np.flip(gray)
-    h, w = gray_flip.shape
-    gaus1 = my_getGKernel((5, 5), 1.6)
-    gaus2 = my_getGKernel((5, 5), 1)
-    gaussian1 = my_filtering(gray_flip, gaus1)
-    gaussian2 = my_filtering(gray_flip, gaus2)
-    DoG = np.zeros_like(gray_flip)
-    for i in range(h):
-        for j in range(w):
-            DoG[i][j] = float(gaussian1[i][j]) - float(gaussian2[i][j])
-    result = np.array(DoG).reshape(1, h, w)
-    result = (result - np.min(result)) / (np.max(result) - np.min(result))
+    result = get_pixel_data(name)
+    result = np.flip(result)
     return result
 
 
-def calc_convolution(X, filters, pad=1):
+def calc_convolution(X, filters):
     _, h, w = X.shape
     filters = np.flip(filters)
     filter_c, filter_h, filter_w = filters.shape
 
-    out_h = (h + 2 * pad - filter_h)
-    out_w = (w + 2 * pad - filter_w)
+    out_h = h
+    out_w = w
 
-    in_X = np.pad(X, [(0, 0), (pad, pad), (pad, pad)], 'constant')
+    in_X = my_padding(X[0], (filter_h, filter_w))
     out = np.zeros((out_h, out_w))
 
     for c in range(filter_c):
@@ -122,7 +77,7 @@ def calc_convolution(X, filters, pad=1):
             for w in range(out_w):
                 w_start = w
                 w_end = w_start + filter_w
-                out[h, w] = np.sum(in_X[c, h_start:h_end, w_start:w_end] * filters[c])
+                out[h, w] = np.sum(in_X[h_start:h_end, w_start:w_end] * filters[c])
 
     return out
 
@@ -157,7 +112,7 @@ def finalResult(name, icon_name):
     for index in indices:
         ax.add_patch(
             patches.Rectangle(
-                (index[1], index[0]),
+                (index[1] - target.shape[2] // 2, index[0] - target.shape[1] // 2),
                 target.shape[2], target.shape[1],
                 edgecolor='cyan',
                 fill=False,
